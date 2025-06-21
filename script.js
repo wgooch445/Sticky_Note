678d5f93b296bdedde0c2bf2ff96bc1291996ac0
wgooch445 <willgooch445@gmail.com>
2025-06-20 21:39:50 -0400

    add delete and edit functionality

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ const notesContainer = document.getElementById('notes-container');
 function addNote() {
   const note = document.createElement('div');
   note.classList.add('note');
+  note.contentEditable = true;       // makes notes editable
   note.textContent = 'new sticky note';
   const rotation = Math.random() * 10 - 5;
   note.style.setProperty('--rotation', `${rotation}deg`);
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+
+  deleteButton.addEventListener('click'), () => {
+    notesContainer.removeChild(note);
+  });
+
+  note.appendChild(deleteButton); // adding delete button inside note
   notesContainer.appendChild(note);
 }
 
